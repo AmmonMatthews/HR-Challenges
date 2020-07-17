942579fb794bc138169e0ca80f2eb4fc177d786a
Ammon Matthews <29493963+AmmonMatthews@users.noreply.github.com>
2020-07-16 22:12:48 -0600

    Create removeNthNodeFromEnd.py

---
+++ removeNthNodeFromEnd.py
@@ -0,0 +1,17 @@
+#Leet Code Challenge
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        size = 1 #size of the list
+        p = head #previous pointer
+        cur = head #current pointer
+        while cur.next: #while a next node exists
+            size += 1 #increment the size
+            cur = cur.next #move the current node over one
+            if size > n + 1: # if size becomes greater than the index of the node to be taken from the end
+                p = p.next #assign th previous pointer to the next node
+        if size == n: #edge case if there is only two values in the list
+            return head.next #return the next node in the list
+        else:
+            p.next = p.next.next #deletes the node from the desired index
+            return head #return head of the list
